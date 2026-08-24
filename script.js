a61204b7b53b756cc84100d4fb63b9f0c7b9ddad
smackinnsean-ai <smackinnsean@gmail.com>
2026-08-24 12:57:02 -0400

    Simplify SLOANE project intake

--- script.js
+++ script.js
@@ -2,6 +2,21 @@ const form = document.querySelector("#project-form");
 const formPanel = document.querySelector("#inquiry-form-panel");
 const successPanel = document.querySelector("#inquiry-success");
 const sendAnotherButton = document.querySelector("#send-another");
+const projectTypeSelect = form.elements.projectType;
+const otherProjectField = document.querySelector("#other-project-field");
+const otherProjectInput = form.elements.otherProjectType;
+
+const syncOtherProjectField = () => {
+  const showOtherField = projectTypeSelect.value === "other";
+  otherProjectField.hidden = !showOtherField;
+  otherProjectInput.required = showOtherField;
+
+  if (!showOtherField) {
+    otherProjectInput.value = "";
+  }
+};
+
+projectTypeSelect.addEventListener("change", syncOtherProjectField);
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -13,5 +28,6 @@ sendAnotherButton.addEventListener("click", () => {
   successPanel.hidden = true;
   formPanel.hidden = false;
   form.reset();
+  syncOtherProjectField();
   form.elements.name.focus();
 });
